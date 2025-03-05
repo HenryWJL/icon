ce7278131c5911619d32677e27b4c3b3fd0b4d8e
HenryWJL <12112921@main.sustech.edu.cn>
2025-03-05 18:17:32 +0800

    update

--- scripts/train_idt.sh
+++ scripts/train_idt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python scripts/train.py task=open_box algo=icon_diffusion_transformer
-python scripts/train.py task=take_lid_off_saucepan algo=icon_diffusion_transformer
+# python scripts/train.py task=open_box algo=icon_diffusion_transformer
+# python scripts/train.py task=take_lid_off_saucepan algo=icon_diffusion_transformer
 python scripts/train.py task=close_drawer algo=icon_diffusion_transformer
 python scripts/train.py task=play_jenga algo=icon_diffusion_transformer
 python scripts/train.py task=open_microwave algo=icon_diffusion_transformer
--- scripts/train_idu.sh
+++ scripts/train_idu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-python scripts/train.py task=put_rubbish_in_bin algo=icon_diffusion_unet
 python scripts/train.py task=open_box algo=icon_diffusion_unet
 python scripts/train.py task=take_lid_off_saucepan algo=icon_diffusion_unet
-python scripts/train.py task=open_microwave algo=icon_diffusion_unet
 python scripts/train.py task=close_drawer algo=icon_diffusion_unet
+python scripts/train.py task=play_jenga algo=icon_diffusion_unet
+python scripts/train.py task=open_microwave algo=icon_diffusion_unet
+python scripts/train.py task=put_rubbish_in_bin algo=icon_diffusion_unet
