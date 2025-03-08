a98f7623894508ebebaf5d7e63a941b99e203ec6
HenryWJL <12112921@main.sustech.edu.cn>
2025-03-08 20:05:57 +0800

    update

--- scripts/train_idt.sh
+++ scripts/train_idt.sh
@@ -2,7 +2,9 @@
 
 # python scripts/train.py task=open_box algo=icon_diffusion_transformer
 # python scripts/train.py task=take_lid_off_saucepan algo=icon_diffusion_transformer
-python scripts/train.py task=close_drawer algo=icon_diffusion_transformer
-python scripts/train.py task=play_jenga algo=icon_diffusion_transformer
-python scripts/train.py task=open_microwave algo=icon_diffusion_transformer
-python scripts/train.py task=put_rubbish_in_bin algo=icon_diffusion_transformer
+# python scripts/train.py task=close_drawer algo=icon_diffusion_transformer
+# python scripts/train.py task=play_jenga algo=icon_diffusion_transformer
+# python scripts/train.py task=open_microwave algo=icon_diffusion_transformer
+# python scripts/train.py task=put_rubbish_in_bin algo=icon_diffusion_transformer
+python scripts/train.py task=open_box algo=icon_diffusion_transformer algo.policy.obs_encoder.image_encoder.backbone.temperature=0.1
+python scripts/train.py task=open_box algo=icon_diffusion_transformer algo.policy.obs_encoder.image_encoder.backbone.temperature=0.07
