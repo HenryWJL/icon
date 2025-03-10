ebfaf025088b0a907c88781fe1f32453639f0508
HenryWJL <12112921@main.sustech.edu.cn>
2025-03-10 19:35:37 +0800

    update

--- scripts/train_dt.sh
+++ scripts/train_dt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python scripts/train.py task=open_box algo=diffusion_transformer
-python scripts/train.py task=take_lid_off_saucepan algo=diffusion_transformer
-python scripts/train.py task=close_drawer algo=diffusion_transformer
-python scripts/train.py task=play_jenga algo=diffusion_transformer
-python scripts/train.py task=open_microwave algo=diffusion_transformer
-python scripts/train.py task=put_rubbish_in_bin algo=diffusion_transformer
+python scripts/train.py task=open_box algo=diffusion_transformer train.seed=0
+python scripts/train.py task=take_lid_off_saucepan algo=diffusion_transformer train.seed=0
+python scripts/train.py task=close_drawer algo=diffusion_transformer train.seed=0
+python scripts/train.py task=play_jenga algo=diffusion_transformer train.seed=0
+python scripts/train.py task=open_microwave algo=diffusion_transformer train.seed=0
+python scripts/train.py task=put_rubbish_in_bin algo=diffusion_transformer train.seed=0
 
