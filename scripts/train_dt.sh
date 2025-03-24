126669374b2e3a09cd7e296739b1b66fbca770c7
HenryWJL <12112921@main.sustech.edu.cn>
2025-03-24 17:35:29 +0800

    update

--- scripts/train_dt.sh
+++ scripts/train_dt.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-python scripts/train.py task=open_box algo=diffusion_transformer train.seed=100
+python scripts/train.py task=open_box algo=diffusion_transformer train.seed=0 train.lr_scheduler.num_warmup_steps=1000
+python scripts/train.py task=close_drawer algo=diffusion_transformer train.seed=0 train.lr_scheduler.num_warmup_steps=1000
+python scripts/train.py task=open_box algo=diffusion_transformer train.seed=100 train.lr_scheduler.num_warmup_steps=1000
 python scripts/train.py task=take_lid_off_saucepan algo=diffusion_transformer train.seed=100
-python scripts/train.py task=close_drawer algo=diffusion_transformer train.seed=100
-python scripts/train.py task=play_jenga algo=diffusion_transformer train.seed=100
-python scripts/train.py task=open_microwave algo=diffusion_transformer train.seed=100
-python scripts/train.py task=put_rubbish_in_bin algo=diffusion_transformer train.seed=100
+python scripts/train.py task=close_drawer algo=diffusion_transformer train.seed=100 train.lr_scheduler.num_warmup_steps=1000
+# python scripts/train.py task=play_jenga algo=diffusion_transformer train.seed=100
+# python scripts/train.py task=open_microwave algo=diffusion_transformer train.seed=100
+# python scripts/train.py task=put_rubbish_in_bin algo=diffusion_transformer train.seed=100
 
--- scripts/train_idt.sh
+++ scripts/train_idt.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-python scripts/train.py task=open_box algo=icon_diffusion_transformer train.seed=100
+python scripts/train.py task=open_box algo=icon_diffusion_transformer train.seed=0 train.lr_scheduler.num_warmup_steps=1000
+python scripts/train.py task=close_drawer algo=icon_diffusion_transformer train.seed=0 train.lr_scheduler.num_warmup_steps=1000
+python scripts/train.py task=open_box algo=icon_diffusion_transformer train.seed=100 train.lr_scheduler.num_warmup_steps=1000
 python scripts/train.py task=take_lid_off_saucepan algo=icon_diffusion_transformer train.seed=100
-python scripts/train.py task=close_drawer algo=icon_diffusion_transformer train.seed=100
-python scripts/train.py task=play_jenga algo=icon_diffusion_transformer train.seed=100
-python scripts/train.py task=open_microwave algo=icon_diffusion_transformer train.seed=100
-python scripts/train.py task=put_rubbish_in_bin algo=icon_diffusion_transformer train.seed=100
+python scripts/train.py task=close_drawer algo=icon_diffusion_transformer train.seed=100 train.lr_scheduler.num_warmup_steps=1000
+# python scripts/train.py task=play_jenga algo=icon_diffusion_transformer train.seed=100
+# python scripts/train.py task=open_microwave algo=icon_diffusion_transformer train.seed=100
+# python scripts/train.py task=put_rubbish_in_bin algo=icon_diffusion_transformer train.seed=100
