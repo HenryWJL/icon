885e76351e6361d34b5d2036e48ed4e14116fd4a
HenryWJL <wangjunlin131@gmail.com>
2025-03-26 16:58:55 +0800

    update

--- scripts/train_dt.sh
+++ scripts/train_dt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 python scripts/train.py task=pick_place_cereal algo=diffusion_transformer
-python scripts/train.py task=stack_cube algo=diffusion_transformer
 python scripts/train.py task=open_door algo=diffusion_transformer train.seed=0
 python scripts/train.py task=pick_place_cereal algo=diffusion_transformer train.seed=0
-python scripts/train.py task=stack_cube algo=diffusion_transformer train.seed=0
 python scripts/train.py task=open_door algo=diffusion_transformer train.seed=100
 python scripts/train.py task=pick_place_cereal algo=diffusion_transformer train.seed=100
+python scripts/train.py task=stack_cube algo=diffusion_transformer
+python scripts/train.py task=stack_cube algo=diffusion_transformer train.seed=0
 python scripts/train.py task=stack_cube algo=diffusion_transformer train.seed=100
--- scripts/train_idt.sh
+++ scripts/train_idt.sh
@@ -2,10 +2,10 @@
 
 python scripts/train.py task=open_door algo=icon_diffusion_transformer
 python scripts/train.py task=pick_place_cereal algo=icon_diffusion_transformer
-python scripts/train.py task=stack_cube algo=icon_diffusion_transformer
 python scripts/train.py task=open_door algo=icon_diffusion_transformer train.seed=0
 python scripts/train.py task=pick_place_cereal algo=icon_diffusion_transformer train.seed=0
-python scripts/train.py task=stack_cube algo=icon_diffusion_transformer train.seed=0
 python scripts/train.py task=open_door algo=icon_diffusion_transformer train.seed=100
 python scripts/train.py task=pick_place_cereal algo=icon_diffusion_transformer train.seed=100
+python scripts/train.py task=stack_cube algo=icon_diffusion_transformer
+python scripts/train.py task=stack_cube algo=icon_diffusion_transformer train.seed=0
 python scripts/train.py task=stack_cube algo=icon_diffusion_transformer train.seed=100
