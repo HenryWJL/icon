77dc7ec4cdecaa7c648355d9e8a576dbb08779ed
HenryWJL <12112921@main.sustech.edu.cn>
2025-04-01 10:47:24 +0800

    update

--- scripts/train_idu.sh
+++ scripts/train_idu.sh
@@ -15,10 +15,10 @@ python scripts/train.py task=stack_cube algo=icon_diffusion_unet train.seed=100
 
 # python scripts/train.py task=open_box algo=icon_diffusion_unet
 # python scripts/train.py task=take_lid_off_saucepan algo=icon_diffusion_unet
-# python scripts/train.py task=close_drawer algo=icon_diffusion_unet
-# python scripts/train.py task=open_box algo=icon_diffusion_unet train.seed=0
-# python scripts/train.py task=take_lid_off_saucepan algo=icon_diffusion_unet train.seed=0
-# python scripts/train.py task=close_drawer algo=icon_diffusion_unet train.seed=0
-# python scripts/train.py task=open_box algo=icon_diffusion_unet train.seed=100
-# python scripts/train.py task=take_lid_off_saucepan algo=icon_diffusion_unet train.seed=100
-# python scripts/train.py task=close_drawer algo=icon_diffusion_unet train.seed=100
+# python scripts/train.py task=close_drawer algo=icon_diffusion_unet train.device=cuda:7
+# python scripts/train.py task=open_box algo=icon_diffusion_unet train.seed=0 train.device=cuda:7
+# python scripts/train.py task=take_lid_off_saucepan algo=icon_diffusion_unet train.seed=0 train.device=cuda:7
+# python scripts/train.py task=close_drawer algo=icon_diffusion_unet train.seed=0 train.device=cuda:7
+# python scripts/train.py task=open_box algo=icon_diffusion_unet train.seed=100 train.device=cuda:7
+# python scripts/train.py task=take_lid_off_saucepan algo=icon_diffusion_unet train.seed=100 train.device=cuda:7
+# python scripts/train.py task=close_drawer algo=icon_diffusion_unet train.seed=100 train.device=cuda:7
