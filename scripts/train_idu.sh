0618cce224398761585de8eb9bcbc423bc55a75f
HenryWJL <12112921@main.sustech.edu.cn>
2025-04-12 10:58:21 +0800

    update

--- scripts/train_idu.sh
+++ scripts/train_idu.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-python scripts/train.py task=open_box algo=icon_diffusion_unet algo.policy.obs_encoder.image_encoder.backbone.enable_fps=false
-python scripts/train.py task=open_box algo=icon_diffusion_unet algo.policy.obs_encoder.image_encoder.backbone.enable_weighted_loss=false
-python scripts/train.py task=open_box algo=icon_diffusion_unet algo.policy.obs_encoder.image_encoder.backbone.enable_fps=false algo.policy.obs_encoder.image_encoder.backbone.enable_weighted_loss=false
-
+python scripts/train.py task=open_box algo=icon_diffusion_unet algo.policy.obs_encoder.image_encoder.backbone.enable_fps=false train.seed=0
+python scripts/train.py task=open_box algo=icon_diffusion_unet algo.policy.obs_encoder.image_encoder.backbone.enable_weighted_loss=false train.seed=0
+python scripts/train.py task=open_box algo=icon_diffusion_unet algo.policy.obs_encoder.image_encoder.backbone.enable_fps=false algo.policy.obs_encoder.image_encoder.backbone.enable_weighted_loss=false train.seed=0
+python scripts/train.py task=open_box algo=icon_diffusion_unet algo.policy.obs_encoder.image_encoder.backbone.enable_fps=false train.seed=100
+python scripts/train.py task=open_box algo=icon_diffusion_unet algo.policy.obs_encoder.image_encoder.backbone.enable_weighted_loss=false train.seed=100
+python scripts/train.py task=open_box algo=icon_diffusion_unet algo.policy.obs_encoder.image_encoder.backbone.enable_fps=false algo.policy.obs_encoder.image_encoder.backbone.enable_weighted_loss=false train.seed=100
 
 # python scripts/train.py task=open_box algo=icon_diffusion_unet
 # python scripts/train.py task=take_lid_off_saucepan algo=icon_diffusion_unet
