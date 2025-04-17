c0aa99d07ab7f9a0e45efc34f270084c28dc73c6
HenryWJL <12112921@main.sustech.edu.cn>
2025-04-17 18:48:19 +0800

    update

--- scripts/train_dt.sh
+++ scripts/train_dt.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-python scripts/train.py task=lift_cube algo=crossway_diffusion_unet train.device=cuda:7
-python scripts/train.py task=open_door algo=crossway_diffusion_unet train.device=cuda:7
-python scripts/train.py task=stack_cube algo=crossway_diffusion_unet train.device=cuda:7
-python scripts/train.py task=lift_cube algo=crossway_diffusion_unet train.seed=0 train.device=cuda:7
 python scripts/train.py task=open_door algo=crossway_diffusion_unet train.seed=0 train.device=cuda:7
 python scripts/train.py task=stack_cube algo=crossway_diffusion_unet train.seed=0 train.device=cuda:7
 python scripts/train.py task=lift_cube algo=crossway_diffusion_unet train.seed=100 train.device=cuda:7
--- scripts/train_du.sh
+++ scripts/train_du.sh
@@ -11,12 +11,6 @@
 # python scripts/train.py task=pick_place_cereal algo=diffusion_unet train.seed=100
 # python scripts/train.py task=stack_cube algo=diffusion_unet train.seed=100
 
-python scripts/train.py task=open_box algo=crossway_diffusion_unet
-python scripts/train.py task=take_lid_off_saucepan algo=crossway_diffusion_unet
-python scripts/train.py task=close_drawer algo=crossway_diffusion_unet
-python scripts/train.py task=open_box algo=crossway_diffusion_unet train.seed=0
-python scripts/train.py task=take_lid_off_saucepan algo=crossway_diffusion_unet train.seed=0
-python scripts/train.py task=close_drawer algo=crossway_diffusion_unet train.seed=0
 python scripts/train.py task=open_box algo=crossway_diffusion_unet train.seed=100
 python scripts/train.py task=take_lid_off_saucepan algo=crossway_diffusion_unet train.seed=100
 python scripts/train.py task=close_drawer algo=crossway_diffusion_unet train.seed=100
