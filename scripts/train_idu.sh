ca9cd282909ec7b9f41fbf0aec0588992a28a0a8
HenryWJL <12112921@main.sustech.edu.cn>
2025-04-21 10:56:34 +0800

    update

--- scripts/train_idu.sh
+++ scripts/train_idu.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-python scripts/train.py task=open_box algo=icon_diffusion_unet algo.policy.obs_encoder.image_encoder.backbone.enable_fps=false train.seed=0
-python scripts/train.py task=open_box algo=icon_diffusion_unet algo.policy.obs_encoder.image_encoder.backbone.enable_weighted_loss=false train.seed=0
-python scripts/train.py task=open_box algo=icon_diffusion_unet algo.policy.obs_encoder.image_encoder.backbone.enable_fps=false algo.policy.obs_encoder.image_encoder.backbone.enable_weighted_loss=false train.seed=0
-python scripts/train.py task=open_box algo=icon_diffusion_unet algo.policy.obs_encoder.image_encoder.backbone.enable_fps=false train.seed=100
-python scripts/train.py task=open_box algo=icon_diffusion_unet algo.policy.obs_encoder.image_encoder.backbone.enable_weighted_loss=false train.seed=100
-python scripts/train.py task=open_box algo=icon_diffusion_unet algo.policy.obs_encoder.image_encoder.backbone.enable_fps=false algo.policy.obs_encoder.image_encoder.backbone.enable_weighted_loss=false train.seed=100
+python scripts/train.py task=open_door algo=diffusion_unet train.seed=0 train.device=cuda:5 train.num_epochs=1000 train.val.ckpt_manager.topk=20
+python scripts/train.py task=open_door algo=icon_diffusion_unet train.seed=0 train.device=cuda:5 train.num_epochs=1000 train.val.ckpt_manager.topk=20
+python scripts/train.py task=open_door algo=crossway_diffusion_unet train.seed=0 train.device=cuda:5 train.num_epochs=1000 train.val.ckpt_manager.topk=20
 
 # python scripts/train.py task=open_box algo=icon_diffusion_unet
 # python scripts/train.py task=take_lid_off_saucepan algo=icon_diffusion_unet
