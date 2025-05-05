77aa4f3d8cc42e904d5dab4b4c54aea6aad00492
HenryWJL <wangjunlin131@gmail.com>
2025-05-05 15:28:51 +0800

    update

--- scripts/train_cdu.sh
+++ scripts/train_cdu.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-python scripts/train.py task=pull_cube algo=crossway_diffusion_unet
-python scripts/train.py task=place_sphere algo=crossway_diffusion_unet
-python scripts/train.py task=lift_peg_upright algo=crossway_diffusion_unet
-python scripts/train.py task=peg_insertion_side algo=crossway_diffusion_unet
-python scripts/train.py task=pull_cube algo=crossway_diffusion_unet train.seed=0
-python scripts/train.py task=place_sphere algo=crossway_diffusion_unet train.seed=0
-python scripts/train.py task=lift_peg_upright algo=crossway_diffusion_unet train.seed=0
-python scripts/train.py task=peg_insertion_side algo=crossway_diffusion_unet train.seed=0
-python scripts/train.py task=pull_cube algo=crossway_diffusion_unet train.seed=100
-python scripts/train.py task=place_sphere algo=crossway_diffusion_unet train.seed=100
-python scripts/train.py task=lift_peg_upright algo=crossway_diffusion_unet train.seed=100
-python scripts/train.py task=peg_insertion_side algo=crossway_diffusion_unet train.seed=100
+python scripts/train.py task=put_rubbish_in_bin algo=crossway_diffusion_unet train.device=cuda:5
+python scripts/train.py task=put_rubbish_in_bin algo=crossway_diffusion_unet train.seed=0 train.device=cuda:5
+python scripts/train.py task=put_rubbish_in_bin algo=crossway_diffusion_unet train.seed=100 train.device=cuda:5
