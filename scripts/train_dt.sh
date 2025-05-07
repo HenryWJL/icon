c942731db23cbcf5f07603ecc2aba6ffcdeb41f3
HenryWJL <12112921@main.sustech.edu.cn>
2025-05-07 19:18:38 +0800

    update

--- scripts/train_dt.sh
+++ scripts/train_dt.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-python scripts/train.py task=close_microwave algo=diffusion_transformer train.device=cuda:5
-python scripts/train.py task=close_microwave algo=diffusion_transformer train.seed=0 train.device=cuda:5
-python scripts/train.py task=close_microwave algo=diffusion_transformer train.seed=100 train.device=cuda:5
+python scripts/train.py task=close_microwave algo=diffusion_transformer train.seed=100 train.device=cuda:5 train.lr_scheduler.num_warmup_steps=500
+python scripts/train.py task=close_microwave algo=icon_diffusion_transformer train.seed=100 train.device=cuda:5 train.lr_scheduler.num_warmup_steps=500
