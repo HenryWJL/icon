82580a21cc080ec0ceb1f4460120336b26115df1
HenryWJL <12112921@main.sustech.edu.cn>
2025-05-07 10:54:52 +0800

    update

--- scripts/train_du.sh
+++ scripts/train_du.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# corrupted mask
-python scripts/train.py task=open_door algo=icon_diffusion_unet train.seed=0 train.device=cuda:4
-python scripts/train.py task=stack_cube algo=diffusion_unet train.seed=100 train.device=cuda:4 train.epoch=100 train.batch_size=16 train.val.ckpt_manager.val_freq=5 train.val.ckpt_manager.topk=20
+python scripts/train.py task=close_microwave algo=diffusion_unet train.device=cuda:4
+python scripts/train.py task=close_microwave algo=diffusion_unet train.seed=0 train.device=cuda:4
+python scripts/train.py task=close_microwave algo=diffusion_unet train.seed=100 train.device=cuda:4
--- scripts/train_idt.sh
+++ scripts/train_idt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# approximate mask
-python scripts/train.py task=open_door algo=icon_diffusion_unet train.seed=0 train.device=cuda:6
-python scripts/train.py task=stack_cube algo=icon_diffusion_unet train.seed=100 train.device=cuda:6 train.epoch=100 train.batch_size=16 train.val.ckpt_manager.val_freq=5 train.val.ckpt_manager.topk=20
+python scripts/train.py task=close_microwave algo=icon_diffusion_transformer
+python scripts/train.py task=close_microwave algo=icon_diffusion_transformer train.seed=0
+python scripts/train.py task=close_microwave algo=icon_diffusion_transformer train.seed=100
