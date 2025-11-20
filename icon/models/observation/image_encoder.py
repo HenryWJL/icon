11068cf8afb7d6f1a39f7bebf3aaa8c86fd9285f
Junlin Wang <12112921@mail.sustech.edu.cn>
2025-11-19 20:37:29 -0500

    Add mask reconstruction and positional embedding methods
    Refactor IConViT class to include mask reconstruction and positional embedding generation.

--- icon/models/observation/image_encoder.py
+++ icon/models/observation/image_encoder.py
@@ -26,7 +26,8 @@ class ViT(VisionTransformer):
         x = x[:, 0]
         return x
 
-# With feature selection
+
+# With mask reconstruction
 class IConViT(ViT):
 
     def __init__(
@@ -57,7 +58,29 @@ class IConViT(ViT):
         self.enable_fps = enable_fps
         self.enable_multi_level_contrast = enable_multi_level_contrast
         self.gamma = gamma
-        self.mask_logits = nn.Parameter(torch.zeros(kwargs['embed_dim']))
+
+        self.image_size = kwargs['img_size'] 
+
+    def init(self) -> None:
+        self.decoder_embed = nn.Linear(384, 512)
+        decode_dims = [64, 128]
+        decode_pe_dim = 64
+        in_out_dims = list(zip(decode_dims[:-1], decode_dims[1:]))
+        self.image_decoder = nn.ModuleList([])
+        for idx, (in_dim, out_dim) in enumerate(reversed(in_out_dims)):
+            is_last = idx >= (len(in_out_dims))
+            self.image_decoder.append(
+                nn.Sequential(
+                    ResidualBlock2D(out_dim + decode_pe_dim, in_dim, 5, 8),
+                    ResidualBlock2D(in_dim, in_dim, 5, 8),
+                    Upsample2d(in_dim) if not is_last else nn.Identity()
+                )
+            )
+        out_channels = 1
+        self.final_conv = nn.Sequential(
+            Conv2dBlock(decode_dims[0], decode_dims[0], 5, 8),
+            nn.Conv2d(decode_dims[0], out_channels, 1),
+        )
 
     def patchify(self, x: Tensor) -> Tensor:
         height, width = x.shape[-2:]
@@ -65,6 +88,21 @@ class IConViT(ViT):
         assert height == width and height % patch_size == 0
         x = rearrange(x, 'b c (h p) (w q) -> b (h w) (p q c)', p=patch_size, q=patch_size)
         return x
+    
+    def generate_positional_embedding(self, x: Tensor, dim: int) -> Tensor:
+        b, _, h, w = x.shape
+        hidx = torch.linspace(-1, 1, steps=h)
+        widx = torch.linspace(-1, 1, steps=w)
+        freq = dim // 4
+        sh = [(2 ** i) * torch.pi * hidx for i in range(freq)]
+        sw = [(2 ** i) * torch.pi * widx for i in range(freq)]
+        grids = [torch.stack(torch.meshgrid(hi, wi, indexing='ij'), axis=0) for hi, wi in zip(sh, sw)]
+        phases = torch.concat(grids, 0)
+        assert phases.shape == (dim // 2, h, w)
+        pe = torch.concat([torch.sin(phases), torch.cos(phases)], axis=0)
+        bpe = pe.unsqueeze(0).repeat(b, 1, 1, 1)
+        bpe = bpe.to(x.device)
+        return bpe
     
     def forward_loss(self, x: Tensor, mask: Tensor) -> Tensor:
         """
@@ -76,53 +114,27 @@ class IConViT(ViT):
         Returns:
             loss (torch.Tensor): the contrastive loss.
         """
-        tokens = x[:, 1:]
-        # Apply hard feature masks
-        scores = torch.sigmoid(self.mask_logits)  
-        topk_indices = torch.topk(scores, 128).indices
-        hard_mask = torch.zeros_like(scores)
-        hard_mask[topk_indices] = 1.0
-        # straight-through estimator: use hard mask in forward, soft in backward
-        feature_mask = hard_mask + (scores - scores.detach())
-        tokens = tokens * feature_mask
-        # Count masked and unmasked tokens
-        count_mask = mask.sum(dim=1).unsqueeze(1)
-        count_unmask = (1.0 - mask).sum(dim=1).unsqueeze(1)
-        # Obtain queries corresponding to masked and unmasked regions.
-        eps = 1e-6
-        query_mask = (tokens * mask.unsqueeze(-1)).sum(dim=1) / (count_mask + eps)
-        query_unmask = (tokens * (1.0 - mask.unsqueeze(-1))).sum(dim=1) / (count_unmask + eps)
-        if self.enable_fps:
-            key_unmask = farthest_point_sample(tokens, num_samples=self.num_samples_unmask, masks=1.0 - mask)
-            key_mask = farthest_point_sample(tokens, num_samples=self.num_samples_mask, masks=mask)
-        else:
-            key_unmask, key_mask = random_sample(tokens, mask, self.num_samples_mask, self.num_samples_unmask)
-        # Compute contrastive losses.
-        loss_unmask = info_nce_loss(
-            query=query_unmask,
-            pos_key=key_unmask,
-            neg_key=key_mask,
-            temp=self.temperature
-        )
-        loss_mask = info_nce_loss(
-            query=query_mask,
-            pos_key=key_mask,
-            neg_key=key_unmask,
-            temp=self.temperature
-        )
-        # Losses are computed on batches with enough samples.
-        flag = torch.logical_and(count_mask >= self.num_samples_mask, count_unmask >= self.num_samples_unmask).float()
-        loss = ((loss_unmask + loss_mask) * flag).sum() / flag.sum()
-        return loss
+        mid_ld = self.decoder_embed(x[:, 0])
+        mid_rgb = mid_ld.reshape(mid_ld.shape[0], -1, 2, 2)
+        # Mask reconstruction
+        n_upsamples = len(self.image_decoder)
+        h_res = w_res = self.image_size // (2 ** n_upsamples)
+        h_scale = w_scale = math.ceil(h_res / 2)
+        x = mid_rgb.repeat(1, 1, h_scale, w_scale)
+        x = x[:, :, :h_res, :w_res]
+        for block in self.image_decoder:
+            pos_embed = self.generate_positional_embedding(x, 64)
+            x = torch.cat([x, pos_embed], dim=1)
+            x = block(x)
+        mask_recons = self.final_conv(x)
+        recons_loss = F.mse_loss(mask_recons, mask)
+        return recons_loss
         
     def forward(self, x: Tensor, mask: Union[Tensor, None] = None) -> Union[Tensor, Tuple]:
         if mask is None:
             return super().forward(x)
         else:
-            # For each mask patch, it is assigned a value of 1 if there are more masked pixels
-            # than unmasked pixels; otherwise, assigned a value of 0.
-            mask = self.patchify(mask.unsqueeze(1))
-            mask = (mask.sum(dim=-1) > self.patch_embed.patch_size[0] ** 2 / 2).float()
+            mask = mask.unsqueeze(1)
             x = self.patch_embed(x)
             cls_token = self.cls_token.expand(x.shape[0], -1, -1)
             x = torch.cat([cls_token, x], dim=1)
@@ -140,7 +152,6 @@ class IConViT(ViT):
                 loss = self.forward_loss(x, mask)
             x = self.norm(x)
             x = x[:, 0]
-            return x, loss
 
 
 # class IConViT(ViT):
